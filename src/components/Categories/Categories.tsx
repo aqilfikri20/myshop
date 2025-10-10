bcf817688918061c201223575aa401d852e0a601
aqilfikri20 <mhdaqilalfikri@gmail.com>
2025-10-10 22:30:49 +0700

    Update Commit

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,121 +1,143 @@
 import React from "react";
 import "./categories.css";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Categories(){
     return(
         <div className="categories">
             <div className="text-popular"><p> Popular Categories</p></div>
             <div className="wrap-categories">
-                <div className="category">
-                    <div className="border-category">
-                    <Image
-                        src="/images/games.png"
-                        alt="games"
-                        width={100} 
-                        height={100} />
+                <Link href="/products" className="link-category">
+                    <div className="category">
+                        <div className="border-category">
+                        <Image
+                            src="/images/games.png"
+                            alt="games"
+                            width={100}
+                            height={100} />
+                        </div>
+                        <div ><p>Gaming</p></div>
                     </div>
-                    <div ><p>Gaming</p></div>
-                </div>
+                </Link>
 
-                <div className="category">
-                    <div className="border-category">
-                    <Image
-                        src="/images/games.png"
-                        alt="games"
-                        width={100} 
-                        height={100} />
-                    </div>
-                    <div><p>Gaming</p></div>
-                </div>
 
-                <div className="category">
-                    <div className="border-category">
-                    <Image
-                        src="/images/games.png"
-                        alt="games"
-                        width={100} 
-                        height={100} />
+                <Link href="/products" className="link-category">
+                    <div className="category">
+                        <div className="border-category">
+                        <Image
+                            src="/images/games.png"
+                            alt="games"
+                            width={100}
+                            height={100} />
+                        </div>
+                        <div><p>Gaming</p></div>
                     </div>
-                    <div><p>Gaming</p></div>
-                </div>
+                </Link>
 
-                <div className="category">
-                    <div className="border-category">
-                    <Image
-                        src="/images/games.png"
-                        alt="games"
-                        width={100} 
-                        height={100} />
+                <Link href="/products" className="link-category">
+                    <div className="category">
+                        <div className="border-category">
+                        <Image
+                            src="/images/games.png"
+                            alt="games"
+                            width={100}
+                            height={100} />
+                        </div>
+                        <div><p>Gaming</p></div>
                     </div>
-                    <div><p>Gaming</p></div>
-                </div>
+                </Link>
 
-                <div className="category">
-                    <div className="border-category">
-                    <Image
-                        src="/images/games.png"
-                        alt="games"
-                        width={100} 
-                        height={100} />
+                <Link href="/products" className="link-category">
+                    <div className="category">
+                        <div className="border-category">
+                        <Image
+                            src="/images/games.png"
+                            alt="games"
+                            width={100}
+                            height={100} />
+                        </div>
+                        <div><p>Gaming</p></div>
                     </div>
-                    <div><p>Gaming</p></div>
-                </div>
+                </Link>
 
-                <div className="category">
-                    <div className="border-category">
-                    <Image
-                        src="/images/games.png"
-                        alt="games"
-                        width={100} 
-                        height={100} />
+                <Link href="/products" className="link-category">
+                    <div className="category">
+                        <div className="border-category">
+                        <Image
+                            src="/images/games.png"
+                            alt="games"
+                            width={100}
+                            height={100} />
+                        </div>
+                        <div><p>Gaming</p></div>
                     </div>
-                    <div><p>Gaming</p></div>
-                </div>
+                </Link>
 
-                <div className="category">
-                    <div className="border-category">
-                    <Image
-                        src="/images/games.png"
-                        alt="games"
-                        width={100} 
-                        height={100} />
+                <Link href="/products" className="link-category">
+                    <div className="category">
+                        <div className="border-category">
+                        <Image
+                            src="/images/games.png"
+                            alt="games"
+                            width={100}
+                            height={100} />
+                        </div>
+                        <div><p>Gaming</p></div>
                     </div>
-                    <div><p>Gaming</p></div>
-                </div>
+                </Link>
 
-                <div className="category">
-                    <div className="border-category">
-                    <Image
-                        src="/images/games.png"
-                        alt="games"
-                        width={100} 
-                        height={100} />
+                <Link href="/products" className="link-category">
+                    <div className="category">
+                        <div className="border-category">
+                        <Image
+                            src="/images/games.png"
+                            alt="games"
+                            width={100}
+                            height={100} />
+                        </div>
+                        <div><p>Gaming</p></div>
                     </div>
-                    <div><p>Gaming</p></div>
-                </div>
+                </Link>
 
-                <div className="category">
-                    <div className="border-category">
-                    <Image
-                        src="/images/games.png"
-                        alt="games"
-                        width={100} 
-                        height={100} />
+                <Link href="/products" className="link-category">
+                    <div className="category">
+                        <div className="border-category">
+                        <Image
+                            src="/images/games.png"
+                            alt="games"
+                            width={100}
+                            height={100} />
+                        </div>
+                        <div><p>Gaming</p></div>
                     </div>
-                    <div><p>Gaming</p></div>
-                </div>
+                </Link>
 
-                <div className="category">
-                    <div className="border-category">
-                    <Image
-                        src="/images/games.png"
-                        alt="games"
-                        width={100} 
-                        height={100} />
+                <Link href="/products" className="link-category">
+                    <div className="category">
+                        <div className="border-category">
+                        <Image
+                            src="/images/games.png"
+                            alt="games"
+                            width={100}
+                            height={100} />
+                        </div>
+                        <div><p>Gaming</p></div>
                     </div>
-                    <div><p>Gaming</p></div>
-                </div>
+                </Link>
+
+                <Link href="/products" className="link-category">
+                    <div className="category">
+                        <div className="border-category">
+                        <Image
+                            src="/images/games.png"
+                            alt="games"
+                            width={100}
+                            height={100} />
+                        </div>
+                        <div><p>Gaming</p></div>
+                    </div>
+                </Link>
                 
             </div>
 
