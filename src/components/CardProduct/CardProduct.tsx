88f10d726fa4103368512cda8e99b5c58ca670ed
aqilfikri20 <mhdaqilalfikri@gmail.com>
2025-10-10 22:02:52 +0700

    Update Commit

--- src/components/CardProduct/CardProduct.tsx
+++ src/components/CardProduct/CardProduct.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import "./card-product.css";
 import Image from "next/image";
-import { FaRegHeart, FaStar } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa6";
 
 
 export default function CardProduct( {name, price} : {name: string, price: string} ){
     return(
         <div className="card-product">
             <div className="wrap-image-product">
-                <div className="image-product"></div>
-                <button className="like-product"><FaRegHeart /></button>
+                <div className="image-product">
+                <Image
+                    src="/products/laptop1.jpg"
+                    alt="product"
+                    fill
+                    style={{
+                        objectFit: "cover"
+                    }}
+                />
+                </div>
+                <button className="like-product"><FaHeart /></button>
             </div>
             <div className="name-product">{name}</div>
             <div className="price-product">{price}</div>
