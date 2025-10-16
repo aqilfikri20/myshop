30c0632ec249048ee143d855c2f6179aa6c80a3b
aqilfikri20 <mhdaqilalfikri@gmail.com>
2025-10-17 00:01:41 +0700

    Update

---
+++ src/app/AddStorePage/page.jsx
@@ -0,0 +1,9 @@
+import AddStore from "../../components/AddStore/AddStore";
+
+export default  function AddStorePage() {
+    return(
+        <div>
+            <AddStore />
+        </div>
+    )
+}
