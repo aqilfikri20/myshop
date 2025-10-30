318064ec45fc220562d16dab902ec799292cdd67
aqilfikri20 <mhdaqilalfikri@gmail.com>
2025-10-30 23:58:46 +0700

    Update

--- src/components/DashboardOwner/DashboardAdmin.jsx
+++
@@ -1,7 +0,0 @@
-export default function DashboardAdmin() {
-    return(
-        <div>
-            <h1>Halaman Admin Store</h1>
-        </div>
-    )
-}
---
+++ src/components/DashboardOwner/DashboardOwner.jsx
@@ -0,0 +1,12 @@
+export default function DashboardOwner() {
+    return(
+        <div>
+            <h1>Selamat Datang Pemilik Toko</h1>
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+            <div></div>
+        </div>
+    )
+}
