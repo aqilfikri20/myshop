ec77f9882ae4a39c9f4d829de4071a1dfa888c75
aqilfikri20 <mhdaqilalfikri@gmail.com>
2025-11-03 21:39:05 +0700

    Update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
 //login page
 
+import GoogleLogo from "@/components/GoogleLogo/GoogleLogo";
 import LoginInput from "@/components/Login/LoginInput";
 import Link from "next/link";
 
@@ -7,7 +8,7 @@ export default function Login(){
     return(
         <div className="login">
             <LoginInput/>
-            <Link href="http://localhost:4000/api/auth/google/login">Login dengan Google</Link>
+            <div className="login-google"><Link href="http://localhost:4000/api/auth/google/login"><GoogleLogo/>Login dengan Google</Link></div>
             <p>Belum punya akun? Lakukan <Link href="/register">Register</Link></p>
         </div>
     )
